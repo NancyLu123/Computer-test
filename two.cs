42fc8a4c154a224709a189c18d7aeba0066daa7c
Nancy Lu <nancyapple168168@gmail.com>
2021-10-04 10:22:33 +0800

    no message

--- two.cs
+++ two.cs
@@ -1,60 +1,26 @@
 using System;
-using System.Collections.Generic;
-using System.Collections;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace C
 {
-    class first
+    class two
     {
-         static void Main(string[] args)
+        static void Main(string[] args)
         {   
-            int k,k1;
-            Console.WriteLine("請輸入大小:\n（必須大於等於2，小於等於103）");
-            int length=int.Parse( Console.ReadLine() );
-            Console.WriteLine("請輸入一整數：\n（必須介於-109~109之間）");
-            int target=int.Parse( Console.ReadLine() );
+			//二、資料處理-字串
+            string topic = "人易科技:上 機 測 驗 - 演算法!";
 
-            int[] b = new int[length];
-            Console.WriteLine(target);
-
-            Console.WriteLine("請輸入"+length+"個整數值到陣列裡：\n(數字大小必須介於-109~109之間)");
-
-            for (k = 0; k < length; k++)
-            {
-
-            Console.Write("第{0}個陣列元素:b[{1}]=", k + 1, k);
-            b[k] = int.Parse( Console.ReadLine() );
-            }
-
-            for (k1 = 0; k1 < length; k1++)
-            {
-
-            Console.WriteLine("b[{0}]={1}", k1, b[k1]);
-            }
-            var res=TwoSum(b,target);
+            //1.字元「:」改成全型)
+            string one = topic.Replace(":","：");
+            Console.Write(one+"\r\n");
             
-            Console.Read();
-            foreach(int value in res) 
-            { 
-                Console.Write(value + " "); 
-            } 
-            
-        }
+            //2.去掉中文字間的空白(保留-號兩側空白)
+            string two = topic.Replace(" ", String.Empty);
+            string[] twoArray=two.Split('-');
+            Console.Write(twoArray[0]+" - "+twoArray[1]+"\r\n");
 
-         public int[] TwoSum(int[] nums, int target) {
-            //六、兩數總和
-            int q=nums.length;
-            for(int i=0;i<q;i++){
-                for(int j=0;i<q;j++){
-                    if((nums[i]+nums[j])==target){
-                        return new int[]{i,j};
-                    }
-                }
-            }
-
+            //3.列印出符號：到-之間的字元
+            string[] threeArray = topic.Split(':');
+            string[] threeArray2=threeArray[1].Split('-');
+            Console.Write(threeArray2[0]);
         }
     }
 }
